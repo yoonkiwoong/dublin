b72b7892766d312e9e921c5632bbdc98f1965069
YOONKIWOONG <realeyed@gmail.com>
2019-01-01 21:56:33 +0900

    반납 항목을 하나의 목록으로 추가

--- routes/rental.js
+++ routes/rental.js
@@ -38,9 +38,36 @@ router.post(['/:uid/rental'], function (req, res) {
         };
     });
 });
+router.get(['/:uid/return'], function (req, res) {
+    var uid = req.params.uid;
+    db.get('SELECT uid, device_name, device_condition, device_return_user, device_return_dt FROM device WHERE uid = ?', [uid], function (err, devicedb) {
+        if (err) {
+            console.log(err);
+            res.redirect('/error');
+        } else {
+            res.render('./rental/return', { title: '장비 반납', returninfo: devicedb });
+        }
+    });
+});
+
+router.post(['/:uid/return'], function (req, res) {
+    var uid = req.params.uid;
+    var device_return_user = req.body.device_return_user;
+    var device_return_dt = format.asString('yyyy-MM-dd hh:mm', new Date());
+
+    db.run('UPDATE device SET device_condition = 0, device_rental_dt = null, device_return_user = ?, device_return_dt = ? WHERE uid = ?', [device_return_user, device_return_dt, uid], function (err) {
+        if (err) {
+            console.log(err);
+            res.redirect('/error');
+        } else {
+            console.log("RENTAL DB UPDATED");
+            res.redirect('/rental');
+        }
+    });
+});
 
 router.get(['/'], function (req, res) {
-    db.all('SELECT uid, device_name, device_ostype, device_osversion, device_condition FROM device WHERE device_condition = 0', function (err, devicedb) {
+    db.all('SELECT uid, device_name, device_osversion, device_condition, device_rental_user, device_rental_dt FROM device', function (err, devicedb) {
         if (err) {
             console.log(err);
             res.redirect('/error');
