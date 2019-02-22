1c73a37a74d2133d61f167989928863c73fc9f28
YOONKIWOONG <realeyed@gmail.com>
2019-02-22 23:27:45 +0900

    rentalDB 설정을 위한 준비

--- routes/device.js
+++ routes/device.js
@@ -1,31 +1,43 @@
-var express = require('express')
-var router = express.Router()
-var Device = require('../config/devciedb')
+const express = require('express')
+const router = express.Router()
+const Device = require('../config/devciedb')
 
 router.get('/add', function (req, res) {
   res.render('./device/add')
 })
 
 router.post('/add', function (req, res) {
-  let device = new Device()
-
-  device.manufacturer = req.body.manufacturer
-  device.name = req.body.name
-  device.model = req.body.model
-  device.serial = req.body.serial
-  device.imei = req.body.imei
-  device.os_type = req.body.os_type
-  device.os_version = req.body.os_version
-  device.device_get_dt = req.body.get_dt
-  device.assetcode = req.body.assetcode
-  device.info = req.body.info
+  let device = new Device(
+    {
+      manufacturer: req.body.manufacturer,
+      name: req.body.name,
+      model: req.body.model,
+      serial: req.body.serial,
+      imei: req.body.imei,
+      os_type: req.body.os_type,
+      os_version: req.body.os_version,
+      device_get_dt: req.body.get_dt,
+      assetcode: req.body.assetcode,
+      info: req.body.info,
+      rental:
+      [
+        {
+          rental_user_name: null,
+          rental_dt: null,
+          return_user_name: null,
+          return_dt: null
+        }
+      ]
+    }
+  )
 
   device.save(function (err) {
     if (err) {
-      res.redirect('/')
+      console.log(err)
+      res.redirect('/error')
     }
     console.log('DB INSERT DONE')
-    res.redirect('/device/add')
+    res.redirect('/device')
   })
 })
 
@@ -33,17 +45,19 @@ router.get('/:_id/edit', function (req, res) {
   let id = req.params._id
   console.log('EDIT GET ID : ' + id)
 
-  Device.findById(id, function (err, editDb) {
+  Device.findById(id, function (err, device) {
     if (err) {
-      res.redirect('/')
+      console.log(err)
+      res.redirect('/error')
     }
-    console.log('EDIT DB ' + '\n' + editDb)
-    Device.distinct('manufacturer', function (err, manufacturerDb) {
+    console.log('EDIT DB ' + '\n' + device)
+    Device.distinct('manufacturer', function (err, deviceManufacturer) {
       if (err) {
-        res.redirect('/')
+        console.log(err)
+        res.redirect('/error')
       }
-      console.log('MANUFACTURER LIST : ' + manufacturerDb)
-      res.render('./device/edit', { title: '장비 수정', editInfo: editDb, manufacturerInfo: manufacturerDb })
+      console.log('MANUFACTURER LIST : ' + deviceManufacturer)
+      res.render('./device/edit', { title: '장비 수정', editDB: device, manufacturerDB: deviceManufacturer })
     })
   })
 })
@@ -67,7 +81,8 @@ router.post('/:_id/edit', function (req, res) {
     },
     function (err) {
       if (err) {
-        res.redirect('/')
+        console.log(err)
+        res.redirect('/error')
       }
       res.redirect('/device/' + id)
     }
@@ -78,12 +93,13 @@ router.get('/:_id/delete', function (req, res) {
   let id = req.params._id
   console.log('DELETE GET UID : ' + id)
 
-  Device.findById(id, 'name', function (err, deleteDb) {
+  Device.findById(id, 'name', function (err, device) {
     if (err) {
-      res.redirect('/')
+      console.log(err)
+      res.redirect('/error')
     }
-    console.log('DELETE DB : ' + deleteDb)
-    res.render('./device/delete', { title: '장비 삭제', deleteInfo: deleteDb })
+    console.log('DELETE DB : ' + device)
+    res.render('./device/delete', { title: '장비 삭제', deleteDB: device })
   })
 })
 
@@ -93,7 +109,8 @@ router.post('/:_id/delete', function (req, res) {
 
   Device.findByIdAndDelete(id, function (err) {
     if (err) {
-      res.redirect('/')
+      console.log(err)
+      res.redirect('/error')
     }
     res.redirect('/device')
   })
@@ -103,22 +120,24 @@ router.get('/:_id', function (req, res) {
   let id = req.params._id
   console.log('LIST ID : ' + id)
 
-  Device.findOne({ _id: id }, function (err, Device) {
+  Device.findOne({ _id: id }, function (err, device) {
     if (err) {
-      res.redirect('/')
+      console.log(err)
+      res.redirect('/error')
     }
-    console.log('INFO DB : ' + '\n' + Device)
-    res.render('./device/info', { title: '장비 정보', infodb: Device })
+    console.log('INFO DB : ' + '\n' + device)
+    res.render('./device/info', { title: '장비 정보', infoDB: device })
   })
 })
 
 router.get('/', function (req, res) {
-  Device.find({}, function (err, docs) {
+  Device.find({}, function (err, device) {
     if (err) {
-      res.redirect('/')
+      console.log(err)
+      res.redirect('/error')
     }
-    console.log('LIST DB : ' + '\n' + docs)
-    res.render('./device/list', { title: '장비 목록', devicedb: docs })
+    console.log('LIST DB : ' + '\n' + device)
+    res.render('./device/list', { title: '장비 목록', listDB: device })
   })
 })
 
--- routes/rental.js
+++ routes/rental.js
@@ -1,87 +1,108 @@
-var express = require('express');
-var router = express.Router();
-var format = require('date-format');
+const express = require('express')
+const router = express.Router()
+const format = require('date-format')
+const Device = require('../config/devciedb')
 
-var db = require('../config/database');
+router.get('/:_id/rental', function (req, res) {
+  let id = req.params._id
+  let rentalUserName = req.user.name
+  console.log(`RENTAL USER NAME : ${rentalUserName}`)
 
-router.get(['/:uid/rental'], function (req, res) {
-    var uid = req.params.uid;
-    db.get('SELECT uid, device_name FROM device WHERE uid = ?', [uid], function (err, devicedb) {
-        if (err) {
-            console.log(err);
-            res.redirect('/error');
-        } else {
-            db.all('SELECT uid, user_name FROM user', function (err, userdb) {
-                if (err) {
-                    console.log(err);
-                    res.redirect('/error');
-                } else {
-                    res.render('./rental/rental', { title: '장비 대여', deviceinfo: devicedb, userinfo: userdb });
-                }
-            });
-        }
-    });
-});
+  Device.findById(id, function (err, device) {
+    if (err) {
+      console.log(err)
+      res.redirect('/error')
+    }
+    res.render('./rental/rental', { title: '장비 대여', rentalDB: device, retalUser: rentalUserName })
+  })
+})
 
-router.post(['/:uid/rental'], function (req, res) {
-    var device_uid = req.params.uid;
-    var user_uid = req.body.user_uid;
-    var rental_dt = format.asString('yyyy-MM-dd hh:mm', new Date());
+router.post(['/:_id/rental'], function (req, res) {
+  let id = req.params._id
+  let rentalUserName = req.user.name
+  let rentalDate = format.asString('yyyy-MM-dd hh:mm', new Date())
 
-    db.run('UPDATE rental SET device_uid = ?, user_uid = ?, rental_dt = ?, return_dt = null WHERE device_uid = ?', [device_uid, user_uid, rental_dt, device_uid], function (err) {
-        if (err) {
-            console.log(err)
-            res.redirect('/error');
-        } else if (this.changes == 0) {
-            console.log("RENTAL DB UPDATE FAIL");
-            res.redirect('/rental');
-        } else if (this.changes == 1) {
-            console.log("RENTAL DB UPDATED");
-            res.redirect('/rental');
-        }
-    });
-});
+  Device.findById(id, function (err, device) {
+    let uid = device.rental[0].uid
+    console.log(uid)
+    if (err) {
+      console.log(err)
+      res.redirect('/error')
+    }
+    device.rental.push({
+      rental_user_name: rentalUserName,
+      rental_dt: rentalDate,
+      return_user_name: null,
+      return_dt: null
+    })
+    device.save(function (err) {
+      if (err) {
+        console.log(err)
+        res.redirect('/error')
+      }
+      res.redirect('/rental')
+    })
+  })
+    // var device_uid = req.params.uid;
+    // var user_uid = req.body.user_uid;
+    // var rental_dt = format.asString('yyyy-MM-dd hh:mm', new Date());
 
-router.get(['/:uid/return'], function (req, res) {
-    var uid = req.params.uid;
-    db.get('SELECT uid, device_name FROM device WHERE uid = ?', [uid], function (err, devicedb) {
-        if (err) {
-            console.log(err);
-            res.redirect('/error');
-        } else {
-            res.render('./rental/return', { title: '장비 반납', deviceinfo: devicedb });
-        }
-    });
-});
+    // db.run('UPDATE rental SET device_uid = ?, user_uid = ?, rental_dt = ?, return_dt = null WHERE device_uid = ?', [device_uid, user_uid, rental_dt, device_uid], function (err) {
+    //     if (err) {
+    //         console.log(err)
+    //         res.redirect('/error');
+    //     } else if (this.changes == 0) {
+    //         console.log("RENTAL DB UPDATE FAIL");
+    //         res.redirect('/rental');
+    //     } else if (this.changes == 1) {
+    //         console.log("RENTAL DB UPDATED");
+    //         res.redirect('/rental');
+    //     }
+    // });
+})
 
-router.post(['/:uid/return'], function (req, res) {
-    var device_uid = req.params.uid;
-    var user_uid = req.body.return_user;
-    var return_dt = format.asString('yyyy-MM-dd hh:mm', new Date());
+// router.get(['/:uid/return'], function (req, res) {
+//     var uid = req.params.uid;
+//     db.get('SELECT uid, device_name FROM device WHERE uid = ?', [uid], function (err, devicedb) {
+//         if (err) {
+//             console.log(err);
+//             res.redirect('/error');
+//         } else {
+//             res.render('./rental/return', { title: '장비 반납', deviceinfo: devicedb });
+//         }
+//     });
+// });
 
-    db.run('UPDATE rental SET device_uid = ?, user_uid = ?, rental_dt = null, return_dt = ? WHERE device_uid = ?', [device_uid, user_uid, return_dt, device_uid], function (err) {
-        if (err) {
-            console.log(err)
-            res.redirect('/error');
-        } else if (this.changes == 0) {
-            console.log("RETURN DB UPDATE FAIL");
-            res.redirect('/rental');
-        } else if (this.changes == 1) {
-            console.log("RETURN DB UPDATED");
-            res.redirect('/rental');
-        }
-    });
-});
+// router.post(['/:uid/return'], function (req, res) {
+//     var device_uid = req.params.uid;
+//     var user_uid = req.body.return_user;
+//     var return_dt = format.asString('yyyy-MM-dd hh:mm', new Date());
 
-router.get(['/'], function (req, res) {
-    db.all('SELECT device.uid, device.device_name, device.device_osversion, user.user_name, rental.rental_dt FROM rental LEFT JOIN device ON rental.device_uid=device.uid LEFt JOIN user ON rental.user_uid=user.uid', function (err, listdb) {
-        if (err) {
-            console.log(err);
-            res.redirect('/error');
-        } else {
-            res.render('./rental/list', { title: '대여 목록', listinfo: listdb });
-        };
-    });
-});
+//     db.run('UPDATE rental SET device_uid = ?, user_uid = ?, rental_dt = null, return_dt = ? WHERE device_uid = ?', [device_uid, user_uid, return_dt, device_uid], function (err) {
+//         if (err) {
+//             console.log(err)
+//             res.redirect('/error');
+//         } else if (this.changes == 0) {
+//             console.log("RETURN DB UPDATE FAIL");
+//             res.redirect('/rental');
+//         } else if (this.changes == 1) {
+//             console.log("RETURN DB UPDATED");
+//             res.redirect('/rental');
+//         }
+//     });
+// });
 
-module.exports = router;
+router.get('/', function (req, res) {
+  console.log(`USER : ${req.user}`)
+
+  Device.find({}, function (err, device) {
+    // console.log(device[0].rental.length)
+    if (err) {
+      console.log(err)
+      res.redirect('/error')
+    }
+    res.render('./rental/list', { title: '대여 목록', listDB: device })
+  })
+})
+
+module.exports = router
