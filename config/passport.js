92663b9f1e2ee79e86195f2e207a86351c2a307a
YOONKIWOONG <realeyed@gmail.com>
2019-02-25 01:48:31 +0900

    callbackurl 환경 변수 처리

--- config/passport.js
+++ config/passport.js
@@ -18,7 +18,7 @@ passport.deserializeUser(function (id, done) {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:3030/auth/google/callback'
+  callbackURL: process.env.GOOGLE_CALLBACK_URL
 },
 function (accessToken, refreshToken, profile, done) {
   User.findOne({ google_id: profile.id }, function (err, user) {
